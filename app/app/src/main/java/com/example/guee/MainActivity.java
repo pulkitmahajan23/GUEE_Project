5e768674c0f724ee0111db26768fe56330d91660
pulkitmahajan23 <pulkitmahajan1998@gmail.com>
2021-03-30 00:31:33 +0530

    App init

---
+++ app/app/src/main/java/com/example/guee/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.guee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView=findViewById(R.id.text);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        final DatabaseReference current_ac = database.getReference().child("Current_AC");
+
+        current_ac.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                float value = dataSnapshot.getValue(float.class);
+               // Log.d(TAG, "Value is: " + value);
+
+                textView.setText("Ac Current: "+value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+             //   Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+    }
+}
